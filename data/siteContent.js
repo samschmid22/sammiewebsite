4e988fbeb747e69d73e88fc9a816b1f3ab206e43
Samantha Schmid <236088764+samschmid22@users.noreply.github.com>
2025-11-26 13:35:10 -0700

    61

--- data/siteContent.js
+++ data/siteContent.js
@@ -139,7 +139,7 @@ export const projects = [
     bullets: [],
     links: [
       {
-        href: "/docs/the-human-reset.pdf",
+        href: "/docs/thehumanreset.pdf",
         label: "Download PDF",
       },
     ],
